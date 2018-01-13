41b9acc163a2a9e8953e99152464fa164fefd4f3
tgoda <tgoda@tgodanomacbook-pro.local>
2018-01-14 02:20:50 +0900

    検索画面機能およびデザイン修正 ・ピッカーを非選択状態に戻せる機能を追加

--- FallenCherryBlossom/ViewController/CardSearchViewController.swift
+++ FallenCherryBlossom/ViewController/CardSearchViewController.swift
@@ -15,16 +15,16 @@ class CardSearchViewController: UIViewController {
     let megamiList = CharaName.megamiList
     let actionTypeList = ActionType.actionTypeList
     var allCardList = [Card]()
-    let defaultMegamiButtonTitle1 = "メガミ1"
-    let defaultMegamiButtonTitle2 = "メガミ2"
-    let defaultActionTypeButtonTitle = "アクション"
+    var currentSelectedButton: UIButton?    // 非選択状態にするためのメソッドで参照する
+    let defaultSelectionStr = "非選択" // 非選択として扱う際の文字列
     @IBOutlet weak var megamiButton: UIButton!
     @IBOutlet weak var megamiButton2: UIButton!
     @IBOutlet weak var actionTypeButton: UIButton!
 
     @IBAction func onTappedMegamiButton(_ sender: UIButton) {
+        currentSelectedButton = sender
         let strMegamiList = megamiList.map({ $0.rawValue })
-        let picker = ActionSheetStringPicker(title: defaultMegamiButtonTitle1, rows: strMegamiList,
+        let picker = ActionSheetStringPicker(title: defaultSelectionStr, rows: strMegamiList,
                                              initialSelection: 0,
                                              doneBlock: { (picker, value, index) in
                                                 self.megamiButton.setAttributedTitle((index as? String)?.getSearchButtonFont(),
@@ -32,37 +32,45 @@ class CardSearchViewController: UIViewController {
         },
                                              cancel: nil , origin: sender)
 
+        picker?.addCustomButton(withTitle: defaultSelectionStr, actionBlock: {
+            sender.setAttributedTitle(self.defaultSelectionStr.getSearchButtonFont(), for: .normal)
+            self.view.layoutIfNeeded()
+        })
         picker?.show()
     }
 
     @IBAction func onTappedActionTypeButton(_ sender: UIButton) {
+        currentSelectedButton = sender
         let actionTypeStrList = actionTypeList.map({ $0.rawValue })
-        let picker = ActionSheetStringPicker(title: defaultActionTypeButtonTitle,
+        let picker = ActionSheetStringPicker(title: defaultSelectionStr,
                                              rows: actionTypeStrList,
                                              initialSelection: 0,
                                              doneBlock: { (picker, value, index) in
                                                 self.actionTypeButton.setAttributedTitle((index as? String)?.getSearchButtonFont(),
                                                                                          for: .normal)
         }, cancel: nil, origin: sender)
+        picker?.addCustomButton(withTitle: defaultSelectionStr, actionBlock: {
+            sender.setAttributedTitle(self.defaultSelectionStr.getSearchButtonFont(), for: .normal)
+            self.view.layoutIfNeeded()
+        })
         picker?.show()
     }
 
     @IBAction func onTappedSearchButton(_ sender: UIButton) {
-//        var selectedCardList = allCardList
         var selectedMegami1 = [Card]()
 
-        if megamiButton.titleLabel?.text != defaultMegamiButtonTitle1 {
+        if megamiButton.titleLabel?.text != defaultSelectionStr {
             selectedMegami1 = allCardList.filter({ $0.megamiName == megamiButton.titleLabel?.text })
         }
 
         var selectedMegami2 = [Card]()
-        if megamiButton.titleLabel?.text != defaultMegamiButtonTitle2 {
+        if megamiButton2.titleLabel?.text != defaultSelectionStr {
             selectedMegami2 = allCardList.filter({ $0.megamiName == megamiButton2.titleLabel?.text })
         }
         
         var mergedMegamiCardList = selectedMegami1 + selectedMegami2
         
-        if actionTypeButton.titleLabel?.text != defaultActionTypeButtonTitle {
+        if actionTypeButton.titleLabel?.text != defaultSelectionStr {
             mergedMegamiCardList = mergedMegamiCardList.filter({ $0.mainType == actionTypeButton.titleLabel?.text ||
                 $0.subType == actionTypeButton.titleLabel?.text
             })
@@ -77,8 +85,9 @@ class CardSearchViewController: UIViewController {
     }
 
     @IBAction func onTappedMegami2Button(_ sender: UIButton) {
+        currentSelectedButton = sender
         let strMegamiList = megamiList.map({ $0.rawValue })
-        let picker = ActionSheetStringPicker(title: defaultMegamiButtonTitle2, rows: strMegamiList,
+        let picker = ActionSheetStringPicker(title: defaultSelectionStr, rows: strMegamiList,
                                              initialSelection: 0,
                                              doneBlock: { (picker, value, index) in
                                                 self.megamiButton2.setAttributedTitle((index as? String)?.getSearchButtonFont(),
@@ -86,10 +95,19 @@ class CardSearchViewController: UIViewController {
         },
                                              cancel: nil , origin: sender)
         
+        picker?.addCustomButton(withTitle: defaultSelectionStr, actionBlock: {
+            sender.setAttributedTitle(self.defaultSelectionStr.getSearchButtonFont(), for: .normal)
+            self.view.layoutIfNeeded()
+        })
         picker?.show()
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(true)
+        currentSelectedButton = nil
     }
 }
